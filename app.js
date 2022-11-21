3c1f7d6e105d40c213359ea23c93c133c20b0b80
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-21 06:13:27 -0400

    Explicacion de la clase de martes

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const express = require('express')
 //? Initial configs
 
 const port = 9000
-const app = expres()
+const app = express()
 //?Habilitar recibir formato JSON
 app.use(express.json())
 
 const todoDB = []
-let id = 1
+//? TODO
+// {
+//     id:5,
+//     title: 'Practicar express',
+//     description: 'Ver videos de apoyo y crear otra API',
+//     is_completed: false
+// }
+let id = 0
 
 app.get('/', (req, res) => {
     res.json({
@@ -17,12 +24,31 @@ app.get('/', (req, res) => {
     })
 })
 
-//? /todo /todo/:id
 
+
+//? /todo /todo/:id
+//? Rutas de TODO's
 app.get('/todo', (req, res) =>{
-    
+    res.status(200).json(todoDB)
 })
 
+app.post('/todo', (req, res) => {
+    const {title, description} = req.body
+
+    if(title && description){
+        const newTodo = {
+            id: id++,
+            title,
+            description,
+            is_completed: false
+        }
+        todoDB.push(newTodo)
+        res.status(200).json(newTodo)
+    } else{
+        res.status(400).json({message: 'Invalid data'})
+    }
+
+})
 
 app.listen(port, () =>{
     console.log(`Server started at port ${port}`)
