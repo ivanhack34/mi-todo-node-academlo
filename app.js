eff91f699885bf8da290ec946d5f07b2bede219d
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-28 01:44:39 -0400

    Agregado de router en app.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 //? dependencia de express
 const express = require('express')
 
-//? Initial configs
+const taskRouter = require('./tasks/tasks.router')
 
+//? Initial configs
 const port = 9000
 const app = express()
 //?Habilitar recibir formato JSON
 app.use(express.json())
-
-const todoDB = []
-//? TODO
-// {
-//     id:5,
-//     title: 'Practicar express',
-//     description: 'Ver videos de apoyo y crear otra API',
-//     is_completed: false
-// }
-let id = 0
 
 app.get('/', (req, res) => {
     res.json({
@@ -24,47 +15,61 @@ app.get('/', (req, res) => {
     })
 })
 
-
-
-//? /todo /todo/:id
-//? Rutas de TODO's
-app.get('/todo', (req, res) =>{
-    res.status(200).json(todoDB)
-})
-
-app.post('/todo', (req, res) => {
-    const {title, description} = req.body
-
-    if(title && description){
-        const newTodo = {
-            id: id++,
-            title,
-            description,
-            is_completed: false
-        }
-        todoDB.push(newTodo)
-        res.status(200).json(newTodo)
-    } else{
-        res.status(400).json({message: 'Invalid data'})
-    }
-
-})
-
-//Buscaremos el valor por si id
-app.get('/todo/:id', (req, res)=> {
-    const id = req.params.id;
-
-    const todo = todoDB.find(item => item.id == id)
-
-    if(todo){
-        res.status(200).json(todo)
-    } else {
-        res.status(404).json({message: 'Invalid ID'})
-    }
-
-
-})
+app.use('/', taskRouter)
 
 app.listen(port, () =>{
     console.log(`Server started at port ${port}`)
 })
+
+//! const todoDB = []
+//? TODO
+// {
+//     id:5,
+//     title: 'Practicar express',
+//     description: 'Ver videos de apoyo y crear otra API',
+//     is_completed: false
+// }
+//! let id = 0
+
+
+
+
+//? /todo /todo/:id
+//? Rutas de TODO's
+//! app.get('/todo', (req, res) =>{
+//!     res.status(200).json(todoDB)
+//! })
+
+//! app.post('/todo', (req, res) => {
+//!     const {title, description} = req.body
+
+//!     if(title && description){
+//!         const newTodo = {
+//!             id: id++,
+//!             title,
+//!             description,
+//!             is_completed: false
+//!         }
+//!         todoDB.push(newTodo)
+//!         res.status(200).json(newTodo)
+//!     } else{
+//!         res.status(400).json({message: 'Invalid data'})
+//!     }
+
+//! })
+
+//Buscaremos el valor por si id
+//! app.get('/todo/:id', (req, res)=> {
+//!     const id = req.params.id;
+
+//!     const todo = todoDB.find(item => item.id == id)
+
+//!     if(todo){
+//!         res.status(200).json(todo)
+//!     } else {
+//!         res.status(404).json({message: 'Invalid ID'})
+//!     }
+
+
+//! })
+
