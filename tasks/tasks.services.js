cf1979f7a6710feee9ed3e84208097bfdc823f37
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-28 01:27:13 -0400

    Inclucion del services en tasks

--- tasks/tasks.services.js
+++ tasks/tasks.services.js
@@ -6,8 +6,34 @@ const taskControllers = require('./tasks.controllers')
 // taskControllers.findAllTodos()
 // taskControllers.findTodoById()
 
+const getAllTodos = (req, res) => {
+    const data = taskControllers.findAllTodos()
+    res.status(200).json(data)
+}
 
+const getTodoById = (req, res) => {
+    const id = req.params.id
+    const data = taskControllers.findTodoById(id)
+    if(data){
+        res.status(200).json(data)
+    } else {
+        res.status(404).json({message: 'Invalid ID'})
+    }
+}
 
+const postTodo = (req, res) => {
+    const {title, description} = req.body
 
+    if(title && description){
+        const data = taskControllers.createTodo({tile, description})
+        res.status(201).json(data)
+    }else{
+        res.status(400).json({message: 'Invalid Data', fields: {title: 'String', description: 'String'}})
+    }
+}
 
-
+module.exports = {
+    getAllTodos,
+    getTodoById,
+    postTodo
+}
